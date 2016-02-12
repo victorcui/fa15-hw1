0165bf8e1d0a43a71e1bcae690c98f699240da00
victorcui <cheng.cui@berkeley.edu>
2016-02-11 22:42:29 -0800

    done hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,9 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+  	int_array = a.map(&:to_i)
+  	int_array = int_array.map{|e| e = e.to_i + 2}.select { |e| (e % 2 == 0) && (e <= 10)}
+  	return int_array.uniq.inject(:+)
+
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,18 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  return (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  return a.sort.map{ |e| e+=1 }
+
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return "#{first_name} #{last_name}"
+
 end
 
 def blockin_time(a)
